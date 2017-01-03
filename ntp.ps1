db66fe0d036294822c3d26017c0cee404a366317
brandongraves08 <brandongraves08@gmail.com>
2017-01-03 15:43:39 -0600

    Add files via upload

---
+++ ntp.ps1
@@ -0,0 +1,8 @@
+﻿$Hosts = get-vmhost 
+ForEach ($Host in $Hosts)
+{
+Remove-VmHostNtpServer -NtpServer "*.*.*.*" -VMHost $Host | Out-Null 
+Add-VmHostNtpServer -NtpServer "*.*.*.*" -VMHost $Host | Out-Null
+Get-VmHostService -VMHost $Host | Where-Object {$_.key -eq "ntpd"} | Restart-VMHostService -Confirm:$false | Out-Null
+write "NTP Server was changed on $Host"
+}
